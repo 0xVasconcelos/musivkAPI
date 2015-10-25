4e12aae6bfcedfd9c4e23f12871aeb7e31c16e97
lucaslg26 <lucaslg26@yahoo.com.br>
2015-10-25 10:33:27 -0300

    TravisCI fix

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ var express = require('express');
 var app = express();
 require('./app/api-routes/routes')(app);
 
+if(process.argv[2]){
+    process.exit();
+}
 
 var server = app.listen(3000, function () {
     var host = server.address().address;
