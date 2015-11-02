d0b704c077798c3e9b996b6742ac43e3b24c36f9
lucaslg26 <lucaslg26@yahoo.com.br>
2015-11-02 19:53:43 -0300

    searchSong method update

--- app/api-routes/routes.js
+++ app/api-routes/routes.js
@@ -9,13 +9,23 @@ module.exports = function(app){
 
     app.get('/api/v1/searchSong/:data/:accessToken', function(req, res) {
         var songNameEnc = encodeURIComponent(req.params.data);
-        var VKresponse;
         request({
-                    url: "https://api.vk.com/method/audio.search?q=" + songNameEnc + "&v=5.37&access_token=" + req.params.accessToken,
+                    url: "https://api.vk.com/method/audio.search?q=" + songNameEnc + "&v=5.37&count=100&access_token=" + req.params.accessToken,
                     json: true
                 }, function (error, response, body) {
                     if (!error && response.statusCode === 200) {
-                        res.json(body.response);
+                        var songList = [];
+                        var responseArray = body.response.items;
+                        for(var i in responseArray){
+                            songList[i] = {
+                                id: responseArray[i].id,
+                                owner_id:  responseArray[i].owner_id,
+                                artist: responseArray[i].artist,
+                                title: responseArray[i].title,
+                                duration: responseArray[i].duration
+                            };
+                        }
+                        res.json(songList);
                     }
                 });
 
